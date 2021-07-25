4c6fdd1d36d4a920e787e4cab76ea13f17245b86
xiaoze <1067632338@qq.com>
2021-07-25 23:56:01 +0800

    handle exception

--- api/src/main/java/com/azu/generate/exception/UnifyException.java
+++ api/src/main/java/com/azu/generate/exception/UnifyException.java
@@ -4,5 +4,10 @@ package com.azu.generate.exception;
  * @author zzs
  * @date 2021/7/20 16:27
  */
-public class UnifyException extends Exception {
+public class UnifyException extends RuntimeException {
+
+    private String errCode;
+    private String errMsg;
+
+
 }
